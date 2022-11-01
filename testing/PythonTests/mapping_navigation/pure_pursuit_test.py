8f1e61cd5205fc46993a87e95257b202364a5810
Nicolas Tuttle <nicolastuttle@cmail.carleton.ca>
2022-11-01 01:56:54 -0400

    test

--- testing/PythonTests/mapping_navigation/pure_pursuit_test.py
+++ testing/PythonTests/mapping_navigation/pure_pursuit_test.py
@@ -1,9 +1,9 @@
 from src.mapping_navigation.scripts.map_information import *
-from src.mapping_navigation.scripts import pure_pursuit
+from src.mapping_navigation.scripts.pure_pursuit import *
 
 
 def car_state_update_pos():
-    state = pure_pursuit.CarState(5)
+    state = CarState(5)
     state.update_pos(Point((2, 2)), (4, 6, 8, 10))
     print(f'{state.rear_x}, {state.rear_y}, {state.yaw}')
 
