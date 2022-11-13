b6b007cf54b021a26e07f23faa81abf74de50353
Phuc La <phucla2201@gmail.com>
2022-11-13 16:37:52 -0500

    Added testing for lidar and move some constant to global in cluster detection

---
+++ testing/PythonTests/lidar_object_recognition_test.py
@@ -0,0 +1,11 @@
+from cluster_detection import ClusterDetection
+
+
+class TestClusterDetection:
+    def test_find_aabb(self):
+        c_detection = ClusterDetection()
+        result = []
+        find_aabb([[1, 2, 3], [3, 4, 5], [6, 7, 8]], result)
+        assert result == [1, 2, 3, 6, 7, 8]
+
+
