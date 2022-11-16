69a37b88a9770ad81f2a4d7fb8d4ef7a6fd456eb
Phuc La <phucla2201@gmail.com>
2022-11-15 20:02:16 -0500

    Fixing one bug in testing

--- testing/PythonTests/lidar_object_recognition_test.py
+++ testing/PythonTests/lidar_object_recognition_test.py
@@ -5,7 +5,7 @@ class TestClusterDetection:
     def test_find_aabb(self):
         c_detection = ClusterDetection()
         result = []
-        find_aabb([[1, 2, 3], [3, 4, 5], [6, 7, 8]], result)
+        c_detection.find_aabb([[1, 2, 3], [3, 4, 5], [6, 7, 8]], result)
         assert result == [1, 2, 3, 6, 7, 8]
 
 
