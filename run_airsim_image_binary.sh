0f0dea196933752f83cdef3a8b14f80a7fe888e0
Nicolas Tuttle <nicolastuttle@cmail.carleton.ca>
2023-03-12 19:10:26 -0400

    Try removing xauth block

--- run_airsim_image_binary.sh
+++ run_airsim_image_binary.sh
@@ -20,20 +20,6 @@ else
     DOCKER_CMD="nvidia-docker run --runtime=nvidia"
 fi
 
-# this block is for running X apps in docker
-XAUTH=/tmp/.docker.xauth
-if [ ! -f $XAUTH ]
-then
-    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z "$xauth_list" ]
-    then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
-    fi
-    chmod a+r $XAUTH
-fi
-
 # this are the first (maximum) four arguments which the user specifies:
 # ex: ./run_airsim_image.sh /PATH/TO/UnrealBinary/UnrealBinary.sh -windowed -ResX=1080 -ResY=720
 # we save them in a variable right now:
@@ -70,8 +56,6 @@ $DOCKER_CMD -it \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    -env="XAUTHORITY=$XAUTH" \
-    --volume="$XAUTH:$XAUTH" \
     --rm \
     $DOCKER_IMAGE_NAME \
     /bin/bash -c "$UNREAL_BINARY_COMMAND"
